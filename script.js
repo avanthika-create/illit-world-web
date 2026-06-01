fcb804e90f4a8f4eedd41a5c9050008be182071f
Avanthika <avanthikasaravanan123@gmail.com>
2026-05-31 23:33:17 -0700

    Added first screen image and button, had to resize and get right position because images are entered very far from eachother. Next, need to work on adding other images and switiching between them.

---
+++ script.js
@@ -0,0 +1,8 @@
+const button = document.getElementById("worldButton");
+const screen= document.getElementById("screen");
+
+button.addEventListener("click", () => { 
+
+    document.querySelector(".computer img").src = 
+    "images/illitcomputer.png"
+});
